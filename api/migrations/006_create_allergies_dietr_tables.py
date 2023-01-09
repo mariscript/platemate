0ff54f0a3bf3b8dc039d6834e9f9180c351884b1
Natalie Tang <natalietang77@gmail.com>
2023-01-09 16:27:52 -0600

    added allergies & diet restrict tables

---
+++ api/migrations/006_create_allergies_dietr_tables.py
@@ -0,0 +1,29 @@
+steps = [
+    [
+        """
+        CREATE TABLE allergies (
+            id SERIAL PRIMARY KEY NOT NULL,
+            seafood BOOLEAN NOT NULL DEFAULT FALSE,
+            gluten_free BOOLEAN NOT NULL DEFAULT FALSE,
+            account_id INT NOT NULL REFERENCES accounts(id)
+        );
+        """,
+        """
+        DROP TABLE allergies;
+        """
+    ],
+    [
+        """
+        CREATE TABLE diet_restrict (
+            id SERIAL PRIMARY KEY NOT NULL,
+            vegan BOOLEAN NOT NULL DEFAULT FALSE,
+            vegetarian BOOLEAN NOT NULL DEFAULT FALSE,
+            halal BOOLEAN NOT NULL DEFAULT FALSE,
+            account_id INT NOT NULL REFERENCES accounts(id)
+        );
+        """,
+        """
+        DROP TABLE diet_restrict;
+        """
+    ]
+]
