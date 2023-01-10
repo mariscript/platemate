d60f970d9b07463b42002aec2277d22891f0f62b
Gina <gejohn793@gmail.com>
2023-01-10 16:29:57 -0600

    reformatted yelp.py

--- api/routers/yelp.py
+++ api/routers/yelp.py
@@ -7,6 +7,7 @@ router = APIRouter()
 def get_yelp(location: str, distance: int, budget: int, unix_time: int, repo: YelpQueries = Depends()):
     return repo.get_yelp(location, distance, budget, unix_time)
 
+
 @router.get('/api/yelp/{id}')
 def get_yelp_by_one(id, repo: YelpQueries = Depends()):
     return repo.get_yelp_by_id(id)
