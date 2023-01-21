acaa1dfd519ba339bc59d75dbfeffc5ae34cea4b
Jason <jasonolefson@gmail.com>
2023-01-21 13:20:18 -0500

    fixed carousel rotations

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -2,10 +2,10 @@ function MainPage() {
   return (
     <>
       <div className="flex justify-center mt-10">
-      <a href="/questionnaire">
-        <button className="inline-block px-6 py-6 bg-[#C26866] text-white font-medium text-xl leading-tight uppercase rounded-full shadow-md hover:bg-[#FDECA9] hover:shadow-lg focus:bg-[#C26866] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#C26866] active:shadow-lg transition duration-150 ease-in-out">
-          Take The Questionnaire!
-        </button>
+        <a href="/questionnaire">
+          <button className="inline-block px-6 py-6 bg-[#C26866] text-white font-medium text-xl leading-tight uppercase rounded-full shadow-md hover:bg-[#FDECA9] hover:shadow-lg focus:bg-[#C26866] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#C26866] active:shadow-lg transition duration-150 ease-in-out">
+            Take The Questionnaire!
+          </button>
         </a>
       </div>
       <div
@@ -13,6 +13,7 @@ function MainPage() {
         className="carousel slide carousel-fade carousel-dark relative"
         data-bs-ride="carousel"
       >
+        {/* indicators */}
         <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
           <button
             data-bs-target="#carouselDarkVariant"
@@ -43,7 +44,9 @@ function MainPage() {
           ></button>
         </div>
 
+        {/* inner */}
         <div className="carousel-inner relative w-full overflow-hidden">
+          {/* item 0 */}
           <div className="carousel-item active relative float-left w-full">
             <img
               src={require("./images/morimoto.jpg")}
@@ -55,7 +58,7 @@ function MainPage() {
               <p className="">PlateMate will help you choose!</p>
             </div>
           </div>
-
+          {/* item 1 */}
           <div className="carousel-item relative float-left w-full">
             <img
               src={require("./images/rebelcheese.jpg")}
@@ -67,7 +70,7 @@ function MainPage() {
               <p>We'll help you find the right plate.</p>
             </div>
           </div>
-
+          {/* item 2 */}
           <div className="carousel-item relative float-left w-full">
             <img
               src={require("./images/SUGARFISH.jpg")}
@@ -79,6 +82,7 @@ function MainPage() {
               <p>No problem, let us know in the questionnaire!</p>
             </div>
           </div>
+          {/* item 3 */}
           <div className="carousel-item relative float-left w-full">
             <img
               src={require("./images/gyukaku.jpg")}
@@ -90,18 +94,7 @@ function MainPage() {
               <p>We'll find the plate for you!</p>
             </div>
           </div>
-          <div className="carousel-item relative float-left w-full">
-            <img
-              src={require("./images/italian.jpg")}
-              className="block w-screen scale-50"
-              alt="Italian Food"
-            />
-            <div className="carousel-caption hidden md:block absolute text-center bg-[#FDECA9]">
-              <h5 className="text-xl">Have restrictions or allergies?</h5>
-              <p>Sign up to edit your profile!</p>
-            </div>
-          </div>
-
+          {/* item 4 */}
           <div className="carousel-item relative float-left w-full">
             <img
               src={require("./images/italian.jpg")}
@@ -115,6 +108,7 @@ function MainPage() {
           </div>
         </div>
 
+        {/* carousel controls */}
         <button
           className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
           type="button"
