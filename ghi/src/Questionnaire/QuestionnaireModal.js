9599624e670dce9aeeed71ba6b6f36166b1d5668
mariscript <marisonmunoz@gmail.com>
2023-01-23 09:52:20 -0500

    updated modal carousel, cleared console errors

--- ghi/src/Questionnaire/QuestionnaireModal.js
+++ ghi/src/Questionnaire/QuestionnaireModal.js
@@ -76,37 +76,37 @@ function QuestionModal() {
 
   return (
     <>
-      <div class="">
+      <div className="">
         <button
           type="button"
-          class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           data-bs-toggle="modal"
           data-bs-target="#exampleModalXl"
         ></button>
       </div>
 
       <div
-        class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+        className="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
         id="exampleModalXl"
-        tabindex="-1"
+        tabIndex="-1"
         aria-labelledby="exampleModalXlLabel"
         aria-modal="true"
         role="dialog"
       >
-        <div class="modal-dialog modal-xl relative w-auto pointer-events-none">
-          <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-            <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-              <h1 className="flex justify-center items-center font-bold mt-5 text-3xl mb-5">
+        <div className="modal-dialog modal-xl relative w-auto pointer-events-none">
+          <div className="modal-content justify-center border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-[#FDECA9] bg-clip-padding rounded-md outline-none text-current">
+            <div className="modal-header flex flex-shrink-0 content-center items-center p-5 border-b border-[#C26866] rounded-t-md">
+              <h1 className="flex flex-col items-center font-bold mt-5 text-3xl mb-5">
                 Let's Find Your Plate!
               </h1>
               <button
                 type="button"
-                class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body relative p-4">
+            <div className="modal-body relative p-4">
               <div>
                 <div
                   id="carouselExampleCaptions"
