49882abcad8daf3cceedd1f633871730dbc4f2d6
mariscript <marisonmunoz@gmail.com>
2023-01-24 01:31:08 -0500

    added a card to show restaurant list error

--- ghi/src/Restaurant/RestaurantList.js
+++ ghi/src/Restaurant/RestaurantList.js
@@ -44,7 +44,7 @@ export default function RestaurantList() {
   };
 
   return (
-    <>
+    <div>
       <h1 className="text-center text-4xl font-normal leading-normal mt-0 mb-2 text-gray-600">
         {" "}
         Happy Plating!
@@ -90,12 +90,37 @@ export default function RestaurantList() {
             </div>
           ))
         ) : (
-          <h1>
-            Whoops! We couldn't find restaurants with your answers. Try the
-            questionnaire again, so we can find your plate!
-          </h1>
+          <div className="flex justify-center">
+            <div className="rounded-lg shadow-lg bg-white max-w-sm">
+              <a href="#!">
+                <img
+                  className="rounded-t-lg"
+                  src={require("../images/chili.gif")}
+                  alt=""
+                />
+              </a>
+              <div className="flex flex-col items-center p-6">
+                <h5 className="text-gray-900 text-xl font-medium mb-2">
+                  Whoops!
+                </h5>
+                <h1>
+                  We couldn't find restaurants with your answers. Make sure to
+                  fill out every question! Try the questionnaire again, so we
+                  can find your plate!
+                </h1>
+                <button
+                  data-bs-toggle="modal"
+                  data-bs-target="#questionnaire"
+                  type="button"
+                  className="px-6 py-2.5 mt-2 bg-[#C26866] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:[#FDECA9] hover:shadow-lg hover:text-black focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Try Another Plate!
+                </button>
+              </div>
+            </div>
+          </div>
         )}
       </div>
-    </>
+    </div>
   );
 }
