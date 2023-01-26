51a0b13904745cbbcf6d062a83dd3f4fdfd62069
Natalie Tang <natalietang77@gmail.com>
2023-01-26 17:15:37 -0600

    edit about

--- ghi/src/Footer/About.js
+++ ghi/src/Footer/About.js
@@ -155,11 +155,10 @@ export default function About() {
                 <div className="p-6">
                   <h5 className="text-lg font-bold mb-4">Natalie Tang</h5>
                   <p className="mb-6">
-                    "Hello! I'm Nat and based in DFW. Building this app has
-                    solidified my interest in front-end development and design.
-                    I am always on the lookout for new food experiences and
-                    believe food brings people together! Also, my favorite food
-                    is sushi!"
+                    "Building this app has solidified my interest in front-end
+                    development and design. I am always on the lookout for new
+                    food experiences and believe food brings people together!
+                    Also, my favorite food is sushi!"
                   </p>
                   <ul className="list-inside flex mx-auto justify-center">
                     <a
