b8f198130687310009751736545899012ef2e50d
blackfire888 <zfmacek@hotmail.com>
2023-01-26 11:21:55 -0500

    updated about page blurb, added more cheese

--- ghi/src/Footer/About.js
+++ ghi/src/Footer/About.js
@@ -43,7 +43,7 @@ export default function About() {
                 </div>
                 <div className="p-6">
                   <h5 className="text-lg font-bold mb-4">Zachary Macek</h5>
-                  <p className="mb-6">My favorite food is crab legs</p>
+                  <p className="mb-6">"I'm Zac. My journey in programming started in early 2022. My favorite part about working on this app, besides the friendships we built along the way (apologies for the cheese, pun intended), was working on the databases and completing the backend functionality. My favorite food is crab legs. "</p>
                   <ul className="list-inside flex mx-auto justify-center">
                     <a
                       href="https://www.linkedin.com/in/zachary-macek/"
