e503d037b105898f17741622b8ae1673523c6f96
mariscript <marisonmunoz@gmail.com>
2023-01-31 17:33:14 -0500

    main.py update

--- api/main.py
+++ api/main.py
@@ -12,31 +12,20 @@ app.include_router(yelp.router, tags=["YelpAPI"])
 app.include_router(allergies.router, tags=["Allergies"])
 app.include_router(diet_restrict.router, tags=["Diet Restrictions"])
 
+origins = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "https://team-4-hack-n-snack.gitlab.io",
+    os.environ.get("REACT_APP_PLATEMATE_API_HOST", None),
+    os.environ.get("CORS_HOST", None),
+    os.environ.get("PUBLIC_URL", None),
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8000",
-        "http://localhost:3000",
-        "https://team-4-hack-n-snack.gitlab.io",
-        os.environ.get("REACT_APP_PLATEMATE_API_HOST", None),
-        os.environ.get("CORS_HOST", None),
-        os.environ.get("PUBLIC_URL", None),
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-# @app.get("/api/launch-details")
-# def launch_details():
-#     return {
-#         "launch_details": {
-#             "year": 2022,
-#             "month": 12,
-#             "day": "9",
-#             "hour": 19,
-#             "min": 0,
-#             "tz:": "PST"
-#         }
-#     }
