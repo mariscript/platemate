cbe54ad707eee34fa42a7dcfe495f8eb136dd550
mariscript <marisonmunoz@gmail.com>
2023-01-31 13:45:29 -0500

    tailwind config

--- ghi/tailwind.config.js
+++ ghi/tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const ignoreTransitionProps = require("postcss-ignore-transition-properties");
+const autoprefixer = require("autoprefixer");
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/tw-elements/dist/js/**/*.js",
-    "./node_modules/react-tailwindcss-select/dist/index.esm.js"
+    "./node_modules/react-tailwindcss-select/dist/index.esm.js",
   ],
   theme: {
     extend: {
       spacing: {
-        '1/2': '50%',
-        '1/3': '33.333333%',
-        '2/3': '66.666667%',
-      }
+        "1/2": "50%",
+        "1/3": "33.333333%",
+        "2/3": "66.666667%",
+      },
     },
   },
-  plugins: [require("tw-elements/dist/plugin")],
+  plugins: [
+    require("tw-elements/dist/plugin"),
+    ignoreTransitionProps(),
+    autoprefixer(/* config */),
+  ],
 };
